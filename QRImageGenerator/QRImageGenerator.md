b7359f7cb877796cd8ed2ad7acc62c7b66d6661b
Moritz Lang <moritz@slashkeys.com>
2016-04-14 19:07:44 +0200

    Add QRImageGenerator

---
+++ QRImageGenerator/QRImageGenerator.md
@@ -0,0 +1,31 @@
+# QRImageGenerator
+
+### Description
+
+`QRImageGenerator` allows you to simply generate an QRCode-Image from a given `String`.
+
+### Usage
+
+The only method in this tiny snippet is called `generateQRCodeImage` and takes one argument `withSize` of type `CGSize`. This argument has a default value of `CGSize(width: 300, height: 300)`.
+
+To create a QRCode-Image, simply call the method on the String you want to generate from:
+
+``` swift
+let id = "123456789"
+if let qrImage = id.generateQRCodeImage() {
+    // Do something with the image
+}
+```
+
+### Specify the size
+
+To specify the size of the image we simply pass in a CGSize to the `withSize`-Parameter:
+
+``` swift
+let id = "123456789"
+if let qrImage = id.generateQRCodeImage(CGSize(width: 500.0, height: 500.0)) {
+    // Do something with the image
+}
+```
+
+Special thanks to [Ben](https://twitter.com/BenchR) for helping me out with this snippet! 😉
---
+++ QRImageGenerator/QRImageGenerator.swift
@@ -0,0 +1,26 @@
+// MARK: QRImageGenerator
+
+extension String {
+    func generateQRCodeImage(withSize size: CGSize = CGSize(width: 300.0, height: 300.0)) -> UIImage? {
+        // Get data from String (self)
+        let data = self.dataUsingEncoding(NSISOLatin1StringEncoding)
+        // Create CIQRCodeGenerator filter
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            // Set both the inputMessage and the inputCorrectionLevel of the filter
+            filter.setValue(data, forKey: "inputMessage")
+            filter.setValue("Q", forKey: "inputCorrectionLevel")
+            // Check for the filters outputImage
+            guard let qrImage = filter.outputImage else {
+                fatalError("Could not generate qr code from text \(self)")
+            }
+            // Scale the image by applying the size argument devided by the image's extent size
+            let scaleX = size.width / qrImage.extent.size.width
+            let scaleY = size.height / qrImage.extent.size.height
+            let transformedImage = qrImage.imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
+
+            return UIImage(CIImage: transformedImage)
+        }
+
+        return nil
+    }
+}
